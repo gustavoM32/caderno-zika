f5bc8669d8a14e6b7c881a55975c3f1401358840
Byakko97 <enriquejh97@gmail.com>
2021-09-13 17:17:28 -0500

    added base_template

---
+++ base_template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0)
+#define pb push_back
+#define mp make_pair
+#define sz(x) int(x.size())
+#define trace(x) cerr << #x << ": " << x <<endl;
+
+typedef long long ll;
+
+int main() {
+
+    return 0;
+}
