f238c8cbc5ed9ed78d6e3cd4c36cabc76dcc1689
Byakko97 <enriquejh97@gmail.com>
2021-09-13 16:47:54 -0500

    added uniform random number in a range function

--- utils/random.cpp
+++ utils/random.cpp
@@ -4,3 +4,8 @@
  */
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int x = rng() % n; // to generante random number
+
+// to generate a uniform random number in the range [a, b]
+ll random(ll a, ll b) { 
+    return uniform_int_distribution<ll> (a, b) (rng); 
+}
