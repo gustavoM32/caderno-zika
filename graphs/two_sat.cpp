bf4d53dfd9d3e55b4eee4a8b56f9134c2015a32f
Byakko97 <enriquejh97@gmail.com>
2021-09-13 18:33:53 -0500

    adding two sat implementation

---
+++ graphs/two_sat.cpp
@@ -0,0 +1,63 @@
+/* Returns true if a given set of conditions of the form a v b is
+ * satisfiable
+ *
+ * n - number of atomic propositions
+ *
+ * Complexity: O(n)
+ */
+
+// Useful when negation of atomic expression is given as a negative number
+int conv(int x) {
+	if(x > 0) return (x - 1) << 1;
+	else return (-x - 1) << 1 | 1;
+}
+ 
+vi adj[N], adjt[N];
+ 
+void addEdge(int u, int v) {
+	adj[u].pb(v);
+	adjt[v].pb(u);
+}
+ 
+// Add the condition a v b
+void addProp(int a, int b) {
+	a = conv(a), b = conv(b);
+	addEdge(a^1, b);
+	addEdge(b^1, a);
+}
+ 
+bool vis[N];
+int comp[N];
+stack<int> order;
+ 
+void toposort(int v) {
+	vis[v] = 1;
+	for(int u: adj[v]) if(!vis[u])
+		toposort(u);
+	order.push(v);
+}
+ 
+void mark(int v, int id) {
+	comp[v] = id;
+	for(int u: adjt[v]) if(comp[u] == -1)
+		mark(u, id);
+}
+ 
+// Call this function after adding al conditions with addProp
+bool twoSat(int n) {
+	for(int i = 0; i < (n << 1); i++)
+		comp[i << 1] = comp[i << 1 | 1] = -1;
+	for(int i = 0; i < (n << 1); i++) if(!vis[i])
+		toposort(i);
+	int cont = 0;
+	for(int i = 0; i < (n << 1); i++) {
+		int v = order.top();
+		order.pop();
+		if(comp[v] == -1)
+			mark(v, cont++);
+	}
+	for(int i = 0; i < n; i++)
+		if(comp[i << 1] == comp[i << 1 | 1])
+			return 0;
+	return 1;
+}
