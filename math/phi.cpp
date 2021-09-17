f03e23c80894d76d291c06f1f5dff4afa4d1430a
Gustavo de Medeiros Carlos <34094096+gustavoM32@users.noreply.github.com>
2021-09-17 05:38:00 -0300

    Fix infinite for loop

--- math/phi.cpp
+++ math/phi.cpp
@@ -6,7 +6,7 @@
  */
 ll phi(ll n) {
     ll res = n;
-    for (ll p = 2; p * p; p++) {
+    for (ll p = 2; p * p <= n; p++) {
         if (n % p == 0) {
             while (n % p == 0) n /= p;
             res -= res / p;
