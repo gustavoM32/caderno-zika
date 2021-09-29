cfb682badaa9030f60bd4a5bbac3d11602c7249b
Byakko97 <enriquejh97@gmail.com>
2021-09-29 13:33:14 -0500

    changed angle range

--- geometry/angular_sweep.cpp
+++ geometry/angular_sweep.cpp
@@ -1,7 +1,7 @@
-/* Alternative version of point_integer with < operator for angular sweep. 
- * The order given by the operator is counter-clockwise starting from the ray
- * (y <= 0, x = 0). This template only contains a subset of operators and 
- * functions of the full points template.
+/* Version of point_integer with < operator for angular sweep. 
+ * The angle range is ]-\pi, \pi] and the template considers that the angle of the point
+ * (0, 0) is 0. This template only contains a the operators needed for the for applying
+ * the angular sort.
  */
 
 struct point { 
@@ -15,16 +15,16 @@ struct point {
         return x*other.y - y*other.x;
     }
     int side() const {
-        return x < 0 || (x == 0 && y > 0);
+        return y > 0 || (y == 0 && x < 0);
+    }
+    bool operator==(const point& other) const {
+        return x == other.x && y == other.y;
     }
 	bool operator<(const point& other) const { // for angular sweep
 		int this_side = side(), other_side = other.side();
         if(this_side != other_side) return this_side < other_side;
+        if(*this == point(0, 0)) return 0;
+        if(other == point(0, 0)) return 1;
         return (*this ^ other) > 0;
     }
-	point rotate(point r) {
-        return point(*this ^ r, *this * r);
-    }
 };
-point ccw90(1, 0);
-point cw90(-1, 0);
--- geometry/angular_sweep.test.cpp
+++ geometry/angular_sweep.test.cpp
@@ -5,14 +5,13 @@
 point p[N];
 
 int main() {
+    fastio;
     int n; cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> p[i].x >> p[i].y;
-        p[i].rotate(ccw90);
     }
     sort(p, p + n);
     for(int i = 0; i < n; i++) {
-        p[i].rotate(cw90);
         cout << p[i].x << " " << p[i].y << endl;
     }
 }
