268e03bcc1e036b5f20e4703b1a275eeb386144b
Byakko97 <enriquejh97@gmail.com>
2021-09-29 00:03:38 -0500

    added convex hull template

---
+++ geometry/convex_hull.cpp
@@ -0,0 +1,26 @@
+/* Finds the convex hull of a given set of points. This templates requires
+ * the struct point defined in point_integer.cpp or in point_double.cpp
+ *
+ * p - (input) vector of points for which the convex hull will be found.
+ * ch - convex hull of `p` in counter-clockwise order.
+ */
+
+void convexHull(vector<point> p) {
+    int n = sz(p);
+	sort(p, p + n);
+	vector<point> low, up;
+	for(int i = 0; i < n; i++) {
+		while(sz(up) >= 2 && !right(up[sz(up)-2], up.back(), p[i]))
+			up.pop_back();
+		up.pb(p[i]);
+		while(sz(low) >= 2 && !left(low[sz(low)-2], low.back(), p[i]))
+			low.pop_back();
+		low.pb(p[i]);
+	}
+    vector<point> ch;
+	for(int i = 0; i < sz(low) - 1; i++)
+        ch.pb(low[i]);
+	for(int i = sz(up) - 1; i >= 1; i--)
+        ch.pb(up[i]);
+    return ch;
+}
