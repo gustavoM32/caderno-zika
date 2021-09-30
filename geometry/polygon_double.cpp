c77fbe38eeca63dfee7f8da0337f2f29971bcff0
Byakko97 <enriquejh97@gmail.com>
2021-09-30 01:15:43 -0500

    added rotating calipers

--- geometry/polygon_double.cpp
+++ geometry/polygon_double.cpp
@@ -5,18 +5,18 @@
  * All operations' time complexity are O(1) unless stated otherwise.
  */
 
-struct polygon{
-	vector<point> p;
-	int n;
+struct polygon {
+    vector<point> p;
+    int n;
     polygon() : n(0) {}
     polygon(vector<point> _p) {
         p = _p;
         n = sz(p);
     }
-	void add(point q) {
-		p.pb(q);
+    void add(point q) {
+        p.pb(q);
         n++;
-	}
+    }
     // If positive, the polygon is in ccw order. It is in cw order otherwise.
     ld orientation() { // O(n)
         ld acum = 0;
@@ -31,14 +31,24 @@ struct polygon{
         if(orientation() < -EPS)
             reverse(p.begin(), p.end());
     }
-	bool has(point q) { // O(log n). The polygon must be convex and in ccw order
-		if(right(p[0], p[1], q) || left(p[0], p[n-1], q)) return 0;
-		int lo = 0, hi = n;
-		while(lo + 1 < hi) {
-			int mid = (lo + hi) >> 1;
-			if(left(p[0], p[mid], q)) lo = mid;
-			else hi = mid;
-		}
-		return !right(p[lo], p[(lo + 1) % n], q);
-	}
+    bool has(point q) { // O(log n). The polygon must be convex and in ccw order.
+        if(right(p[0], p[1], q) || left(p[0], p[n-1], q)) return 0;
+        int lo = 0, hi = n;
+        while(lo + 1 < hi) {
+            int mid = (lo + hi) >> 1;
+            if(left(p[0], p[mid], q)) lo = mid;
+            else hi = mid;
+        }
+        return !right(p[lo], p[(lo + 1) % n], q);
+    }
+    ld calipers() { // O(n). The polygon must be convex and in ccw order.
+        ld ans = 0;
+        for(int i = 0, j = 1; i < n; i++) {
+            point vec_i = p[(i+1)%n] - p[i];
+            while((vec_i ^ (p[(j+1)%n] - p[j])) > EPS) 
+                j = (j + 1) % n;
+            ans = max(ans, p[i].dist(p[j])); // Example with polygon diameter
+        }
+        return ans;
+    }
 };
--- geometry/polygon_integer.cpp
+++ geometry/polygon_integer.cpp
@@ -5,18 +5,18 @@
  * All operations' time complexity are O(1) unless stated otherwise.
  */
 
-struct polygon{
-	vector<point> p;
-	int n;
+struct polygon {
+    vector<point> p;
+    int n;
     polygon() : n(0) {}
     polygon(vector<point> _p) {
         p = _p;
         n = sz(p);
     }
-	void add(point q) {
-		p.pb(q);
+    void add(point q) {
+        p.pb(q);
         n++;
-	}
+    }
     // If positive, the polygon is in ccw order. It is in cw order otherwise.
     ll orientation() { // O(n)
         ll acum = 0;
@@ -28,14 +28,24 @@ struct polygon{
         if(orientation() < 0)
             reverse(p.begin(), p.end());
     }
-	bool has(point q) { // O(log n). The polygon must be convex and in ccw order
-		if(right(p[0], p[1], q) || left(p[0], p[n-1], q)) return 0;
-		int lo = 0, hi = n;
-		while(lo + 1 < hi) {
-			int mid = (lo + hi) >> 1;
-			if(left(p[0], p[mid], q)) lo = mid;
-			else hi = mid;
-		}
-		return !right(p[lo], p[(lo + 1) % n], q);
-	}
+    bool has(point q) { // O(log n). The polygon must be convex and in ccw order
+        if(right(p[0], p[1], q) || left(p[0], p[n-1], q)) return 0;
+        int lo = 0, hi = n;
+        while(lo + 1 < hi) {
+            int mid = (lo + hi) >> 1;
+            if(left(p[0], p[mid], q)) lo = mid;
+            else hi = mid;
+        }
+        return !right(p[lo], p[(lo + 1) % n], q);
+    }
+    ll calipers() { // O(n). The polygon must be convex and in ccw order.
+        ll ans = 0;
+        for(int i = 0, j = 1; i < n; i++) {
+            point vec_i = p[(i+1)%n] - p[i];
+            while((vec_i ^ (p[(j+1)%n] - p[j])) > EPS) 
+                j = (j + 1) % n;
+            ans = max(ans, p[i].dist2(p[j])); // Example with polygon diameter squared
+        }
+        return ans;
+    }
 };
