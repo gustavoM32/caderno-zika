b5730c667e7e104297bb27977ed3576007fc7f64
Byakko97 <enriquejh97@gmail.com>
2021-10-06 14:15:34 -0500

    replicated polygon integer changes in polygon double

--- geometry/polygon_double.cpp
+++ geometry/polygon_double.cpp
@@ -33,13 +33,13 @@ struct polygon {
     }
     bool has(point q) { // O(log n). The polygon must be convex and in ccw order.
         if(right(p[0], p[1], q) || left(p[0], p[n-1], q)) return 0;
-        int lo = 0, hi = n;
+        int lo = 1, hi = n;
         while(lo + 1 < hi) {
             int mid = (lo + hi) >> 1;
-            if(left(p[0], p[mid], q)) lo = mid;
+            if(!right(p[0], p[mid], q)) lo = mid;
             else hi = mid;
         }
-        return !right(p[lo], p[(lo + 1) % n], q);
+        return hi != n ? !right(p[lo], p[hi], q) : dist(p[0], q) < dist2(p[0], p[n-1]) + EPS;
     }
     ld calipers() { // O(n). The polygon must be convex and in ccw order.
         ld ans = 0;
@@ -84,4 +84,19 @@ struct polygon {
         // Finds the extreme point in the direction of the vector
         return extreme([&](point p, point q) {return p * v > q * v + EPS;});
     }
+    void normalize() { // p[0] becomes the lowest leftmost point 
+        rotate(p.begin(), min_element(p.begin(), p.end()), p.end());
+    }
+    polygon	operator +(polygon& other) { // Minkowsky sum
+        vector<point> sum;
+        normalize();
+        other.normalize();
+        ld dir;
+        for(int i = 0, j = 0; i < n || j < other.n; i += dir > -EPS, j = dir < EPS) {
+            sum.pb(p[i % n] + other.p[j % other.n]);
+            dir = (p[(i + 1) % n] - p[i % n]) 
+                    ^ (other.p[(j + 1) % other.n] - other.p[j % other.n]);
+        }
+        return polygon(sum);
+    }        
 };
