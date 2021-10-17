a870f92af2f7a357ddb05712f1d1e3b846490bbd
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2021-10-17 00:01:49 -0300

    Add colors to tester

--- run.sh
+++ run.sh
@@ -6,32 +6,39 @@ then
 	exit
 fi
 
-make $1
+if ! make $1
+then
+	exit
+fi
 
-i=1
-
-while [[ -e "in$i.txt" ]]
+for i in {1..9}
 do
-	echo "===== Test $i ====="
-	echo "-- in$i.txt --"
-	cat "in$i.txt"
-	./$1 < "in$i.txt" > "out$i.txt"
-	if [[ -e "ans$i.txt" ]]
+	if [[ -e "in$i.txt" ]]
 	then
-		echo "-------------"
-		echo
-		echo "Answer                                | Output"
-		if diff -ywBW 80 --color "ans$i.txt" "out$i.txt"
+		echo "===== Test $i ====="
+		echo "-- in$i.txt --"
+		cat "in$i.txt"
+		./$1 < "in$i.txt" > "out$i.txt"
+		if [[ -e "ans$i.txt" ]]
 		then
+			echo "-------------"
 			echo
-			echo "Passed!"
+			echo "Answer                                | Output"
+			if diff -ywBW 80 --color "ans$i.txt" "out$i.txt"
+			then
+				echo
+				echo -e "\e[1m\e[32mPassed!\e[39m\e[0m"
+			else
+				echo
+				echo -e "\e[1m\e[31mFailed\e[39m\e[0m"
+			fi
+		else
+			echo
+			echo "-- out$i.txt --"
+			cat "out$i.txt"
+			echo
+			echo -e "\e[1m\e[33mManual\e[39m\e[0m"
 		fi
-	else
 		echo
-		echo "-- out$i.txt --"
-		cat "out$i.txt"
 	fi
-	echo
-
-	i=$(($i + 1))
 done
