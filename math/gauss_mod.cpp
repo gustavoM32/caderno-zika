5ffd29acd6c60edfe67895780784a5eb40533a11
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2021-10-22 02:20:14 -0300

    Add modular gauss

---
+++ math/gauss_mod.cpp
@@ -0,0 +1,89 @@
+/* Solves a system of linear equations in Z_mod
+ *
+ * Complexity: O(n^3)
+ */
+ll gcdExt(ll a, ll b, ll &x, ll &y) {
+	if (b == 0) {
+		x = 1;
+		y = 0;
+		return a;
+	}
+	ll x1, y1;
+	ll res = gcdExt(b, a % b, x1, y1);
+	x = y1;
+	y = x1 - y1 * (a / b);
+	return res;
+}
+
+ll inv(ll a, ll m = MOD) {
+	ll x, y;
+	ll g = gcdExt(a, m, x, y);
+	if (g != 1) return -1;
+	return (m + x % m) % m;
+}
+
+struct Gauss {
+  int n, m;
+  vector<int> pos;
+  int rank = 0;
+  ll mod;
+  vector<vector<ll>> a;
+
+  Gauss(int n, int m, ll mod, vector<vector<ll>> &a) : n(n), m(m), mod(mod), a(a) {
+    pos.assign(m, -1);
+  }
+
+  /* if a solution exists, it will be stored in ans 
+    0 - no solution
+    1 - unique solution
+    2 - infinite number of solutions */
+  int solve(vector<ll> &ans) {
+    for (int col = 0, row = 0; col < m && row < n; col++) {
+      int sel = row;
+      for (int i = row+1; i < n; i++) {
+        if (a[i][col] > 0) {
+          sel = i;
+          break;
+        }
+      }
+
+      if (a[sel][col] == 0) continue;
+
+      swap(a[sel], a[row]);
+
+      pos[col] = row;
+
+      for (int i = 0; i < n; i++)
+        if (i != row) {
+          ll mult = a[i][col] * inv(a[row][col], mod) % mod;
+          a[i][col] = 0;
+          for (int j = col+1; j <= m; j++) {
+            a[i][j] = (a[i][j] + mod - a[row][j] * mult % mod) % mod;
+          }
+        }
+
+      ++row, ++rank;
+    }
+
+    ans.assign(m, 0);
+
+    bool multiple = false;
+
+    for (int i = 0; i < m; i++) {
+      if (pos[i] != -1) ans[i] = a[pos[i]][m] * inv(a[pos[i]][i], mod) % mod;
+      else multiple = true;
+    }
+
+    for (int i = 0; i < n; i++) {
+      ll sum = 0.0;
+      for (int j = 0; j < m; j++) {
+        sum = (sum + ans[j] * a[i][j] % mod) % mod;
+      }
+      if (sum != a[i][m]) return 0;
+    }
+
+    if (multiple) return 2;
+
+    return 1;
+  }
+};
---
+++ math/gauss_mod.test.cpp
@@ -0,0 +1,47 @@
+#define IGNORE
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define pb push_back
+#define mp make_pair
+#define sz(x) int(x.size())
+#define trace(x) cerr << #x << ": " << x <<endl;
+
+typedef long long ll;
+const ll N = 512;
+const ll INF = 1ll << 61;
+const ll MOD = 1e9 + 7;
+
+#include "gauss_mod.cpp"
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<ll>> a(n, vector<ll>(m+1));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> a[i][j];
+    }
+  }
+
+  for (int i = 0; i < n; i++) cin >> a[i][m];
+    
+  Gauss g(n, m, 998244353, a);
+
+  vector<ll> ans;
+  int s = g.solve(ans);
+
+  if (s == 0) cout << "no solution\n";
+  else {
+    if (s == 1) cout << "unique solution\n";
+    else cout << "multiple solutions\n";
+
+    for (int i = 0; i < m; i++) {
+      cout << ans[i] << " ";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
