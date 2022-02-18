e6e436976a5dcca12a656624cd0443ea9aa5d6dc
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2022-02-17 23:07:31 -0300

    Remove base_template.cpp includes

--- data_structures/fenwick_tree.test.cpp
+++ data_structures/fenwick_tree.test.cpp
@@ -1,7 +1,22 @@
-#include "../base_template.cpp"
-#include "fenwick_tree.cpp"
 #define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+/**********/
+#include <bits/stdc++.h>
+#include "fenwick_tree.cpp"
 
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0)
+#define pb push_back
+#define mp make_pair
+#define sz(x) int(x.size())
+#define trace(x) cerr << #x << ": " << x <<endl;
+
+typedef long long ll;
+
+const ll N = 1e6;
+const ll INF = 1LL << 61;
+const ll MOD = 1e9 + 7;
+/**********/
 int main() {
     int n, q;
     cin >> n >> q;
--- graphs/lca.test.cpp
+++ graphs/lca.test.cpp
@@ -1,6 +1,21 @@
-#include "../base_template.cpp"
-#include "lca.cpp"
 #define PROBLEM "https://judge.yosupo.jp/problem/lca"
+/**********/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0)
+#define pb push_back
+#define mp make_pair
+#define sz(x) int(x.size())
+#define trace(x) cerr << #x << ": " << x <<endl;
+
+typedef long long ll;
+
+const ll N = 1e6;
+const ll INF = 1LL << 61;
+const ll MOD = 1e9 + 7;
+/**********/
+#include "lca.cpp"
 
 int main() {
     fastio;
